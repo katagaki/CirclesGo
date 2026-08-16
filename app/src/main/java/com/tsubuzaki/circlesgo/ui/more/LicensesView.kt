1cb0b46533182a4680ea5385f8a4188e767fab44
katagaki <68231035+katagaki@users.noreply.github.com>
2026-08-16 11:12:33 +0900

    Add missing Ktor and OkHttp attributions and polish licenses view

--- app/src/main/java/com/tsubuzaki/circlesgo/ui/more/LicensesView.kt
+++ app/src/main/java/com/tsubuzaki/circlesgo/ui/more/LicensesView.kt
@@ -1,6 +1,7 @@
 package com.tsubuzaki.circlesgo.ui.more
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -22,44 +23,45 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import com.tsubuzaki.circlesgo.R
 import com.tsubuzaki.circlesgo.state.Unifier
 
-private data class Dependency(val name: String, val license: String)
+private const val APACHE_2_0 =
+    "Licensed under the Apache License, Version 2.0 (the \"License\"); " +
+        "you may not use this file except in compliance with the License. " +
+        "You may obtain a copy of the License at\n\n" +
+        "    http://www.apache.org/licenses/LICENSE-2.0"
+
+private data class Dependency(val name: String, val copyright: String) {
+    val license: String get() = "$copyright\n\n$APACHE_2_0"
+}
 
 private val dependencies = listOf(
     Dependency(
         "AndroidX / Jetpack Compose",
-        "Copyright The Android Open Source Project\n\n" +
-            "Licensed under the Apache License, Version 2.0 (the \"License\"); " +
-            "you may not use this file except in compliance with the License. " +
-            "You may obtain a copy of the License at\n\n" +
-            "    http://www.apache.org/licenses/LICENSE-2.0"
+        "Copyright The Android Open Source Project"
     ),
     Dependency(
         "Kotlin / kotlinx.serialization / kotlinx.coroutines",
-        "Copyright 2010-2024 JetBrains s.r.o. and respective authors and developers\n\n" +
-            "Licensed under the Apache License, Version 2.0 (the \"License\"); " +
-            "you may not use this file except in compliance with the License. " +
-            "You may obtain a copy of the License at\n\n" +
-            "    http://www.apache.org/licenses/LICENSE-2.0"
+        "Copyright 2010-2024 JetBrains s.r.o. and respective authors and developers"
+    ),
+    Dependency(
+        "Ktor",
+        "Copyright 2000-2024 JetBrains s.r.o. and respective authors and developers"
+    ),
+    Dependency(
+        "OkHttp / Okio",
+        "Copyright 2019 Square, Inc."
     ),
     Dependency(
         "Material Components for Android",
-        "Copyright The Android Open Source Project\n\n" +
-            "Licensed under the Apache License, Version 2.0 (the \"License\"); " +
-            "you may not use this file except in compliance with the License. " +
-            "You may obtain a copy of the License at\n\n" +
-            "    http://www.apache.org/licenses/LICENSE-2.0"
+        "Copyright The Android Open Source Project"
     ),
     Dependency(
         "AndroidX Browser (Custom Tabs)",
-        "Copyright The Android Open Source Project\n\n" +
-            "Licensed under the Apache License, Version 2.0 (the \"License\"); " +
-            "you may not use this file except in compliance with the License. " +
-            "You may obtain a copy of the License at\n\n" +
-            "    http://www.apache.org/licenses/LICENSE-2.0"
+        "Copyright The Android Open Source Project"
     )
 )
 
@@ -84,18 +86,23 @@ fun LicensesView(unifier: Unifier) {
             Text(
                 text = stringResource(R.string.attributions_title),
                 style = MaterialTheme.typography.titleMedium,
-                fontWeight = FontWeight.Bold
+                fontWeight = FontWeight.Bold,
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis,
+                modifier = Modifier.weight(1f)
             )
         }
 
         LazyColumn(
             modifier = Modifier.fillMaxSize(),
-            contentPadding = androidx.compose.foundation.layout.PaddingValues(
-                horizontal = 16.dp,
-                vertical = 8.dp
+            contentPadding = PaddingValues(
+                start = 16.dp,
+                end = 16.dp,
+                top = 8.dp,
+                bottom = 48.dp
             )
         ) {
-            items(dependencies, key = { it.name }) { dependency ->
+            items(dependencies) { dependency ->
                 Text(
                     text = dependency.name,
                     style = MaterialTheme.typography.titleSmall,
